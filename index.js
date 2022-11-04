104a26df3bbe5aae3f52b210b3e48376ec5fa543
Lam <lam.tran@aalto.fi>
2022-11-04 15:39:55 +0200

    8.7

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 
   type Query {
@@ -171,6 +172,17 @@ const resolvers = {
       const book = { ...args, id: uuid() }
         books = books.concat(book)
         return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      else {
+        const updatedAuthor = { ...author, born: args.setBornTo }
+        authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+        return updatedAuthor
+      }
     }
   }
 }
