e07a252165d72c69c3ad8bdcf3b8fb2e89ba4938
Lam <lam.tran@aalto.fi>
2022-11-04 13:36:59 +0200

    8.2

--- index.js
+++ index.js
@@ -93,16 +93,26 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => [...new Set(books.map(b => b.author))].length
+    authorCount: () => [...new Set(books.map(b => b.author))].length,
+    allBooks: () => books
   }
 }
 
